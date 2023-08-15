7b20f6b53941f9e54789364383244e070faf8330
kmrraviranjan1 <kmrraviranjan1@gmail.com>
2023-08-15 12:04:36 +0530

    feat: hero section added

--- src/components/homepage/HomePageMain.jsx
+++ src/components/homepage/HomePageMain.jsx
@@ -1,30 +1,46 @@
 import React from "react";
-
+import hero from "../../assets/heroImage.jpg";
 const HomePageMain = () => {
   return (
-    <div className="p-2 px-4 bg-gradient-to-b from-pink-100 to-yellow-100">
-      <main>
-        <article>
+    <div className="bg-gradient-to-b from-pink-100 to-yellow-100">
+      <header className="w-full h-screen bg-[url('https://res.cloudinary.com/raviimagedb/image/upload/v1692079957/heroImage_qa2b4l.jpg')] bg-cover bg-center flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className=" text-center text-5xl text-white font-bold drop-shadow-lg">
+            WELCOME TO
+            <span className="text-amber-500">PHYLOCARE</span>
+          </h1>
+          <p className="mt-5 text-center text-3xl text-stone-950 ">
           Phylocare private limited is a pure Ayurveda company which provides
           pure and natural products.
-        </article>
+          </p>
+          <a
+            className="mt-8 px-12 py-3 bg-gradient-to-r from-amber-500 to-red-600 hover:from-amber-600 hover:to-red-700 text-xl text-white/70 font-semibold drop-shadow-lg rounded-full"
+            href="#our_products"
+          >
+            Explore Products
+          </a>
+        </div>
+      </header>
+      <div className="p-2 px-4">
+
+      <main>
         <article>
-          Phylocare ayurvedic medicine can truly enhance the quality of your life and leads to healthy
-          and happier day.
+          Phylocare ayurvedic medicine can truly enhance the quality of your
+          life and leads to healthy and happier day.
         </article>
       </main>
       <br />
       <div>
-        <h4 className=" underline text-lg">Our range of Products :-</h4>
+        <h4 id='our_products' className=" underline text-lg">Our range of Products :-</h4>
         <br />
         <ul className="list-disc list-inside">
-          <li>Phylocare Hair Oil</li>
+          <li>Phylocare Leffi Hair Oil</li>
           <li>Phylocare Amla Shampoo</li>
           <li>Phylocare Keratin & Argan Pro Hair Conditioner</li>
           <li>Skech Churan</li>
           <li>Phylocare Mahabhangraj Tail</li>
-          <li>Phylocare  Pain Killer Oil</li>
-          <li>Phylocare Pain killer Tablet</li>
+          <li>Phylogesic Pain Killer Oil</li>
+          <li>Phylogesic Pain killer Tablet</li>
           <li>...and many more ayurvedic range products</li>
         </ul>
       </div>
@@ -34,6 +50,8 @@ const HomePageMain = () => {
         prices
       </p>
     </div>
+    </div>
+
   );
 };
 
